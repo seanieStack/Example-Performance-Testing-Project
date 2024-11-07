2cfd64495c3d5b15e5566ba347453b938ee92c7e
Seanie Stack <seaniestack345@gmail.com>
2024-11-07 12:51:09 +0000

    added api doc to code

--- src/main/java/com/ostiasolutions/performancetestapplication/controllers/AccountController.java
+++ src/main/java/com/ostiasolutions/performancetestapplication/controllers/AccountController.java
@@ -2,11 +2,17 @@ package com.ostiasolutions.performancetestapplication.controllers;
 
 import com.ostiasolutions.performancetestapplication.entities.Account;
 import com.ostiasolutions.performancetestapplication.services.AccountService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
+@Validated
 public class AccountController {
 
     private final AccountService accountService;
@@ -15,23 +21,47 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Operation(summary = "Get account by account number", description = "Get account by account number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Account found"),
+            @ApiResponse(responseCode = "404", description = "Account not found"),
+    })
     @GetMapping(value = "/{accountNumber}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
     public Account getAccount(@PathVariable long accountNumber) {
         return accountService.getAccount(accountNumber);
     }
 
+    @Operation(summary = "Create account", description = "Create account from body")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Account created"),
+            @ApiResponse(responseCode = "409", description = "Account already exists")
+    })
     @PostMapping(consumes = "application/json", produces = "application/json")
-    public Account createAccount(@Valid @RequestBody Account account) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public Account createAccount(@RequestBody Account account) {
         return accountService.createAccount(account);
     }
 
+    @Operation(summary = "Delete account by account number", description = "Delete account by account number")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Account deleted"),
+            @ApiResponse(responseCode = "404", description = "Account not found")
+    })
     @DeleteMapping("/{accountNumber}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAccount(@PathVariable long accountNumber) {
         accountService.deleteAccount(accountNumber);
     }
 
+    @Operation(summary = "Update account", description = "Update account from body")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Account updated"),
+            @ApiResponse(responseCode = "404", description = "Account provided not found")
+    })
     @PutMapping(consumes = "application/json", produces = "application/json")
-    public Account updateAccount(@Valid @RequestBody Account account) {
+    @ResponseStatus(HttpStatus.OK)
+    public Account updateAccount(@RequestBody Account account) {
         return accountService.updateAccount(account);
     }
 }
